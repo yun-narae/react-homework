83a836a7d345263f1cf25936efc05169d2ccf40d
seulbinim <skfo0827@naver.com>
2024-08-01 00:45:42 +0900

    - ESLint 플러그인 구성   - react   - react-hooks   - react-refresh

--- vite-custom-template/eslint.config.mjs
+++ vite-custom-template/eslint.config.mjs
@@ -1,13 +1,32 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"]
   },
   {
-    languageOptions: { 
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh,
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser, 
         ...globals.node
@@ -16,4 +35,11 @@ export default [
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ];
