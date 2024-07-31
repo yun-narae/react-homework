ca041d992ad8d4dabcd110d8fb5e0e5770bf7b6c
seulbinim <skfo0827@naver.com>
2024-08-01 00:03:21 +0900

    js 세팅

---
+++ atomic-component/src/data/list.js
@@ -0,0 +1,11 @@
+const listData = {
+    items: [
+      { id: '1', title: '맑음'},
+      { id: '2', title: '비내림'},
+      { id: '3', title: '미세먼지'},
+      { id: '4', title: '천둥번개'},
+      { id: '5', title: '흐림'},
+    ],
+  };
+  
+  export default listData;
